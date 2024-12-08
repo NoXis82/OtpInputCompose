27565b88da46fcaef5cafd14f1c60ada08b890ab
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-12-08 21:37:01 +0300

    add OtpViewModel

---
+++ app/src/main/java/com/noxis/otpinputcompose/OtpViewModel.kt
@@ -0,0 +1,85 @@
+package com.noxis.otpinputcompose
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+private const val VALID_OTP_CODE = "1414"
+
+
+class OtpViewModel: ViewModel() {
+
+    private val _state = MutableStateFlow(OtpState())
+    val state = _state.asStateFlow()
+
+    fun onAction(action: OtpAction) {
+        when(action) {
+            is OtpAction.OnChangeFieldFocused -> {
+                _state.update { it.copy(
+                    focusedIndex = action.index
+                ) }
+            }
+            is OtpAction.OnEnterNumber -> {
+                enterNumber(action.number, action.index)
+            }
+            OtpAction.OnKeyboardBack -> TODO()
+        }
+
+
+    }
+
+    private fun enterNumber(number: Int?, index: Int) {
+        val newCode = state.value.code.mapIndexed { currentIndex, currentNumber ->
+            if(currentIndex == index) {
+                number
+            } else {
+                currentNumber
+            }
+        }
+
+        val wasNumberRemoved = number == null
+
+        _state.update {
+            it.copy(
+                code = newCode,
+                focusedIndex = if(wasNumberRemoved || it.code.getOrNull(index) != null) {
+                    it.focusedIndex
+                } else {
+                    getNextFocusedTextFieldIndex(
+                        currentCode = it.code,
+                        currentFocusedIndex = it.focusedIndex
+                    )
+                },
+                
+            )
+        }
+        }
+
+    private fun getNextFocusedTextFieldIndex(currentCode: List<Int?>, currentFocusedIndex: Int?): Int? {
+        if(currentFocusedIndex == null) {
+            return null
+        }
+
+        if(currentFocusedIndex == 3) {
+            return currentFocusedIndex
+        }
+
+        return getFirstEmptyFieldIndexAfterFocusedIndex(
+            code = currentCode,
+            currentFocusedIndex = currentFocusedIndex
+        )
+    }
+
+    private fun getFirstEmptyFieldIndexAfterFocusedIndex(code: List<Int?>, currentFocusedIndex: Int): Int {
+        code.forEachIndexed { index, number ->
+            if(index <= currentFocusedIndex) {
+                return@forEachIndexed
+            }
+            if(number == null) {
+                return index
+            }
+        }
+        return currentFocusedIndex
+    }
+}
